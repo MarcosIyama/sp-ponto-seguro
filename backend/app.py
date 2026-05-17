5c423f0cdddd723426838a4668a47f23a2648fbb
MarcosIyama <marcosiyama@gmail.com>
2026-05-17 17:34:26 -0300

    Atualizando app.py 2

--- backend/app.py
+++ backend/app.py
@@ -92,4 +92,6 @@ def home():
 # RODAR SERVIDOR
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
